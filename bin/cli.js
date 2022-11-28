2fcff8e4f65fcb6c60e95dbd98d50d5cea0affe1
Oriana Apostoli <78334888+oapo1804@users.noreply.github.com>
2022-11-28 13:08:27 -0500

    finish

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import { roll } from "/lib/roll.js"
+import { roll } from "../lib/roll.js"
+import minimist from 'minimist'; 
 
-const args = require('minimist')(process.argv.slice(2));
+const args = minimist(process.argv.slice(2));
 
 const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
 
 console.log(JSON.stringify(roll(sides,dice,rolls)));
-process.exit(0);
+
